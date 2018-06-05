b744fd106afc3685f345a3e0f0d18c87f1db957a
SandraCB <36990092+SandraCB@users.noreply.github.com>
2018-06-05 08:31:49 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQIgzTN7BhWV3E8pL7Aw3xsfHOcAfYj1q2ChCbA7jZmOrRDCgAwd-k1DTmKc8LKMpBgSy_sY5iC6EJG/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1L1vflpy6eP8TsSUJeIF5v7FClSOTVy8OzgW50X4HXO0/edit#gid=0';
